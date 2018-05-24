ac651282acb29185e7b3efe60dcba6c4eef2c653
haoxin <yhxlele@gmail.com>
2018-05-25 01:14:19 +0800

    runJava script

--- runJava.sh
+++ runJava.sh
@@ -2,3 +2,4 @@
 javac -d "classes" Parser/src/parserdef/*.java
 javac -cp "classes:Parser/lib/poi-3.17/*" -d "classes" Parser/src/handler/*.java
 java -cp "classes:Parser/lib/poi-3.17/*:Parser/lib/poi-3.17/lib/*:Parser/lib/poi-3.17/ooxml-lib/*" handler.SocketServer &
+
